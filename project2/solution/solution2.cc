5dcc54a436938ce4f2c94ee99b4b35fab3cc9cf8
wangchenxi722 <1700013013@pku.edu.cn>
2020-06-11 15:51:07 +0800

    naive implementation

--- project2/solution/solution2.cc
+++ project2/solution/solution2.cc
@@ -3,6 +3,7 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
+#include <map>
 
 #include "IR.h"
 #include "IRMutator.h"
@@ -12,6 +13,103 @@
 #include "parser.h"
 
 using namespace Boost::Internal;
+
+std::string index_pool[3] = {"h", "w", "t"};
+class MyMutator : public IRMutator {
+ public:
+    Expr src_change;
+    Expr dst_change;
+    std::string out;
+    //std::vector<std::string> grad;
+    std::string grad;
+    int index_use;
+    int flag;
+    int transfer;
+    std::map<std::string, Expr> match;
+    Ref<const Index> index_temp;
+    Expr global_index;
+    MyMutator(std::string _out, std::string _grad): IRMutator() {
+        flag = 0;
+        transfer = 0;
+        out = _out;
+        grad = _grad;
+        index_use = 0;
+    }
+    Expr visit(Ref<const Var> op) override {
+        if (op->name == grad) {
+            if (flag == 1) {
+                std::vector<Expr> new_args;
+                for (auto arg : op->args) {
+                    transfer = 1;
+                    new_args.push_back(mutate(arg));
+                }
+                transfer = 0;
+                src_change = Var::make(op->type(), ("d" + grad), new_args, op->shape);
+            }
+            else if (flag == 2){
+                return dst_change;
+            }
+            else {
+                return Var::make(op->type(), ("d" + grad), op->args, op->shape);
+            }
+        }
+        else if (op->name == out) {
+            if (flag == 1) {
+                dst_change = Var::make(op->type(), ("d" + out), op->args, op->shape);
+            }
+            else if (flag == 2){
+                return src_change;
+            }
+            else {
+                return Var::make(op->type(), ("d" + out), op->args, op->shape);
+            }
+        }
+        return IRMutator::visit(op);
+    }
+
+
+    Expr visit(Ref<const Index> op) override {
+        if (transfer == 1) {
+            index_temp = op;
+        }
+        else if (transfer == 2) {
+            match[op->name] = global_index;
+        }
+        if (match.find(op->name) != match.end()) {
+            return match[op->name];
+        }
+        return IRMutator::visit(op);
+    }
+
+    Expr visit(Ref<const Binary> op) override {
+        if (transfer == 1) {
+            if (op->op_type == BinaryOpType::Add) {
+                mutate(op->a);
+                Expr new_index = Index::make(index_temp->type(), index_pool[index_use++], 
+                    index_temp->dom, index_temp->index_type);
+                global_index = Binary::make(index_temp->type(), BinaryOpType::Sub, new_index, index_temp);
+                transfer = 2;
+                mutate(op->b);
+                transfer = 0;
+                return new_index;
+            }
+        }
+        return IRMutator::visit(op);
+    }
+
+    Stmt visit(Ref<const Move> op) override {
+        flag = 1;
+        mutate(op->src);
+        mutate(op->dst);
+        flag = 2;
+        Expr new_src = mutate(op->src);
+        Expr new_dst = mutate(op->dst);
+        flag = 0;
+        return Move::make(new_dst, new_src, op->move_type);
+        //return IRMutator::visit(op);
+    }
+
+};
 
 int solution(int i, std::string path, std::string outpath) {
     std::ifstream is(path, std::ios::binary);
@@ -94,7 +192,7 @@ int solution(int i, std::string path, std::string outpath) {
         main_code.visit_group(&visitor);
 
         // mutator
-        IRMutator mutator;
+        MyMutator mutator(outs[0], grad_to[0]);
         main_code = mutator.mutate(main_code);
 
         // printer
